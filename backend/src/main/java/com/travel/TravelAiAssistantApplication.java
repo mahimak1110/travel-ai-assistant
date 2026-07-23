b038bc97b1c5998b2a63e4e590719eb71dbb486e
Mahima <mahimak1110@gmail.com>
2026-07-23 07:00:16 +0530

    Added Pom Changes

---
+++ backend/src/main/java/com/travel/TravelAiAssistantApplication.java
@@ -0,0 +1,12 @@
+package com.travel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelAiAssistantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelAiAssistantApplication.class, args);
+    }
+}
